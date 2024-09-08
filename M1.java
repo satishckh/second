6c18cc87ef5951376d21f43841991bfc8401caa1
satishckh <satishck312@gmail.com>
2024-09-08 23:02:19 +0530

    Update M1.java

--- M1.java
+++ M1.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class M2 {
 public static void main(String[] args) throws IOException {
 	File f1 = new File("basics.txt");
-	f1.createNewFile();
+	f1.getsupport();
 	System.out.println("done");
 	}
 }
