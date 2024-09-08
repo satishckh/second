6fafdc91d06eba329a9a21229ccfe7464d65f025
satishckh <satishck312@gmail.com>
2024-09-08 23:13:47 +0530

    Update M2.java

--- M2.java
+++ M2.java
@@ -8,9 +8,15 @@ public class M2 {
 public static void main(String[] args) throws IOException {
 	File f1 = new File("text1.txt");
 	boolean b1 = f1.createNewFile();
-	System.out.println(b1);
-	boolean b2 = f1.createNewFile();
+		boolean b2 = f1.createNewFile();
 	System.out.println(b2);
 	System.out.println("done");
+	public static void main(String[] args) throws IOException {
+	File f1 = new File("text1.txt");
+	boolean b1 = f1.createNewFile();
+		boolean b2 = f1.createNewFile();
+	System.out.println(b2);
+	System.out.println("done");
+
 	}
 }
